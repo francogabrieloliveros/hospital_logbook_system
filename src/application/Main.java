ac6fea3117159e20f7ebbc2d9d05ebc945dadd01
tmtuazon <theatuazon000@gmail.com>
2025-11-24 15:43:16 +0800

    Finished Logbook view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,6 @@
 package application;
 	
-import application.pages.StaffPage;
+import application.pages.LogBookViewPage;
 import javafx.application.Application;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -12,9 +12,11 @@ public class Main extends Application {
 		Font.loadFont(getClass().getResourceAsStream("/fonts/Roboto-Regular.ttf"), 14);
 		Font.loadFont(getClass().getResourceAsStream("/fonts/Roboto-Bold.ttf"), 14);
 		Font.loadFont(getClass().getResourceAsStream("/fonts/Roboto-SemiBold.ttf"), 14);
-		StaffPage staffPage = new StaffPage();
-		staffPage.setStageComponents(stage);
-		
+		//StaffPage staffPage = new StaffPage();
+		//staffPage.setStageComponents(stage);
+		stage.setResizable(false);
+		LogBookViewPage logbook = new LogBookViewPage();
+		logbook.setStageComponents(stage);
 	}
 	
 	public static void main(String[] args) {
