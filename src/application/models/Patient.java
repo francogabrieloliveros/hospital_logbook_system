aa50fbbbe61ff571375b998698f85a68b335bc81
randomchaffee <107630700+randomchaffee@users.noreply.github.com>
2025-12-04 23:16:30 +0800

    Add LabExam methods to Patients

--- src/application/models/Patient.java
+++ src/application/models/Patient.java
@@ -1,6 +1,7 @@
 package application.models;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 // patient class
 public class Patient implements HospitalElement {
@@ -12,6 +13,7 @@ public class Patient implements HospitalElement {
 	private LocalDate dob; // date of birth
 	private String sex;
 	private String notes;
+	private ArrayList<LabExam> labExams = new ArrayList<>();
 	
 	// constructor
 	public Patient(Hospital hospital, String name, LocalDate dob, String sex, String notes) {
@@ -26,21 +28,35 @@ public class Patient implements HospitalElement {
 		addLogToHospital("Added new patient");
 	}
 	
+	// Turn info into a string for the list
     @Override
     public String toString() {
-    	return id + " | fullName=" + name + " | dob=" + dob + " | sex=" + sex + " | info=" + notes;
+    	return id + " | fullName=" + name + " | dob=" + dob + " | sex=" + sex + 
+    			" | info=" + notes + " | exams=" + labExams.size();
     }
     
+    // Log to LogBook
 	@Override
 	public void addLogToHospital(String message) {
 		hospital.addLogBook(new LogBook("", "patient", message));
 		
 	}
 
+	// Generate an id for the patient
 	@Override
 	public String generateId() {
 		Patient.lastId++;
 		return "PAT-" + String.format("%04d", Patient.lastId);
+	}
+	
+	// Add a lab exam to this patient
+	public void addLabExam(LabExam exam) {
+		labExams.add(exam);
+	}
+	
+	// Get all lab exams for this patient
+	public ArrayList<LabExam> getLabExams() {
+		return labExams;
 	}
 	
 	// getters and setters
