9c267197258d8987d9f4f0795c645d15f01ec4cf
francogabrieloliveros <francogabrieloliveros@gmail.com>
2025-12-04 18:56:32 +0800

    fixed labrequest setup

--- src/application/models/LabRequest.java
+++ src/application/models/LabRequest.java
@@ -1,13 +1,15 @@
 package application.models;
 
 public class LabRequest implements HospitalElement{
+	
+	public static int lastId = 0;
+	
 	private Hospital hospital;
 	private Patient patient;
 	private String request;
 	private String status;
 	private Staff staff;
 	private String id;
-	private static int counter = 0;
 	
 	public LabRequest(Hospital hospital, Patient patient, String request, String status, Staff staff) {
 		this.hospital = hospital;
@@ -16,27 +18,36 @@ public class LabRequest implements HospitalElement{
 		this.status = status;
 		this.staff = staff;
 		this.id = generateId();
-	}
-
-	@Override
-	public void addLogToHospital(String message) {
-		this.hospital.addLabRequest(this);
+		
+		addLogToHospital("Added new lab request");
 	}
 
 	@Override
 	public String toString() {
-		return String.format("%s | testType=%s | patient=%s | physician=%s | status=%s", this.id, this.request, this.patient.getName(), this.staff.getName(), this.status);
+		return String.format("%s | testType=%s | patient=%s | physician=%s | status=%s", 
+				             this.id, 
+				             this.request, 
+				             this.patient.getName(), 
+				             this.staff.getName(), 
+				             this.status);
 	}
-
+	
+	@Override
+	public void addLogToHospital(String message) {
+		hospital.addLogBook(new LogBook("", "labrequest", message));
+	}
+	
 	@Override
 	public String generateId() {
-		String id = String.format("%04d", LabRequest.counter++);
-		return "LBR-" + id;
+		LabRequest.lastId++;
+		String idNumber = String.format("%04d", Staff.lastId);
+		
+		return "LBR-" + idNumber;
 	}
 	
 	public void update(String status) {
 		this.status = status;
-		addLogToHospital("Updated staff information");
+		addLogToHospital("Updated lab request information");
 	}
 	
 	// Getters
